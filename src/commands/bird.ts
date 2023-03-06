2c9096d794843d5743642f7994cfe2c6d685a9eb
Colin Wilson <github@colinwilson.dev>
2023-03-05 19:30:42 -0500

    feat(command): :sparkles: Create bird command

---
+++ src/commands/bird.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Command } from "sheweny";
+import { CommandInteraction, EmbedBuilder } from "discord.js";
+import type { ShewenyClient } from "sheweny";
+
+export class Bird extends Command {
+  constructor(client: ShewenyClient) {
+    super(client, {
+      name: "bird",
+      type: "SLASH_COMMAND",
+      category: "Misc",
+      description: "Return random bird picture.",
+      cooldown: 3,
+    });
+  }
+
+  async execute(interaction: CommandInteraction) {
+    const request = await axios.get("http://shibe.online/api/birds");
+    const { data } = request;
+    interaction.reply({ content: data[0] });
+  }
+}
