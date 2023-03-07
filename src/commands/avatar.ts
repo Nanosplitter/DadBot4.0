e86e19d5ad74f1a10118e96d4a796411343f4067
Colin Wilson <github@colinwilson.dev>
2023-03-06 22:44:10 -0500

    feat: :sparkles: Add avatar command to get avatar of a user

---
+++ src/commands/avatar.ts
@@ -0,0 +1,21 @@
+import { Command } from "sheweny";
+import type { ShewenyClient } from "sheweny";
+// @ts-ignore
+import type { ContextMenuInteraction } from "discord.js";
+
+export default class extends Command {
+  constructor(client: ShewenyClient) {
+    super(client, {
+      name: "avatar",
+      description: "Get avatar of user",
+      type: "CONTEXT_MENU_USER",
+      category: "fun",
+    });
+  }
+
+  async execute(interaction: ContextMenuInteraction) {
+    return interaction.reply({
+      content: interaction.options.getUser("user")?.displayAvatarURL({ extension: "png", size: 512 }),
+    });
+  }
+}
