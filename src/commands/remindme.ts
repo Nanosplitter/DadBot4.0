73dc511c1ef42967b0a779af5da7119cf39dda64
Colin Wilson <github@colinwilson.dev>
2023-03-07 01:31:04 -0500

    feat: :sparkles: Remindme now adds to new table

--- src/commands/remindme.ts
+++ src/commands/remindme.ts
@@ -2,6 +2,7 @@ import { Command } from "sheweny";
 import type { ShewenyClient } from "sheweny";
 import { CommandInteraction, ApplicationCommandOptionType } from "discord.js";
 import * as chrono from "chrono-node";
+import { addReminder } from "../models/remindme-table";
 
 export default class extends Command {
   constructor(client: ShewenyClient) {
@@ -13,6 +14,12 @@ export default class extends Command {
       cooldown: 1,
       options: [
         {
+          name: "what",
+          type: ApplicationCommandOptionType.String,
+          description: "A phrase describing what you want to be reminded about",
+          required: true,
+        },
+        {
           name: "when",
           type: ApplicationCommandOptionType.String,
           description: "A phrase describing when you want to be reminded",
@@ -23,7 +30,18 @@ export default class extends Command {
   }
 
   async execute(interaction: CommandInteraction) {
+    const what = interaction.options.get("what")?.value;
+
     const when = interaction.options.get("when")?.value;
+
+    if (what == undefined) {
+      await interaction.reply({
+        content: "You must specify what you want to be reminded about",
+        ephemeral: true,
+      });
+      return;
+    }
+
     if (when == undefined) {
       await interaction.reply({
         content: "You must specify when you want to be reminded",
@@ -31,9 +49,11 @@ export default class extends Command {
       });
       return;
     }
+
     //TODO: It hates things like "Christmas", figure out why
     const res = chrono.parseDate(when.toString(), { timezone: "EST" });
     console.log(res);
+    await addReminder(interaction.user.username, interaction.user.id, what.toString(), res, interaction.channelId);
     await interaction.reply({ content: `**${when}**\n${res.toString()}` });
   }
 }
