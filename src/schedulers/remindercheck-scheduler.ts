d57b556656b0499e3e617fe8b3e58f634b467fc1
Colin Wilson <github@colinwilson.dev>
2023-03-08 23:38:08 -0500

    refactor: :loud_sound: Remove log everytime reminder is checked

--- src/schedulers/remindercheck-scheduler.ts
+++ src/schedulers/remindercheck-scheduler.ts
@@ -3,6 +3,6 @@ import { reminderEmitter } from "../emitters/reminder-emitter";
 
 export default function runCronJob() {
   new CronJob("*/5 * * * * *", () => {
-    console.log("Reminder check!");
+    //console.log("Reminder check!");
   }).start();
 }
