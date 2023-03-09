54bd06c42c19740240f5da97668c41fe62574f8a
Colin Wilson <github@colinwilson.dev>
2023-03-09 14:18:47 -0500

    feat: :sparkles: Create Fox command

---
+++ src/commands/fox.ts
@@ -0,0 +1,22 @@
+import axios from "axios";
+import { Command } from "sheweny";
+import { CommandInteraction, EmbedBuilder } from "discord.js";
+import type { ShewenyClient } from "sheweny";
+
+export class Fox extends Command {
+  constructor(client: ShewenyClient) {
+    super(client, {
+      name: "fox",
+      type: "SLASH_COMMAND",
+      category: "Misc",
+      description: "Return random fox picture.",
+      cooldown: 0,
+    });
+  }
+
+  async execute(interaction: CommandInteraction) {
+    const request = await axios.get("https://randomfox.ca/floof/");
+    const { data } = request;
+    interaction.reply({ content: data.image });
+  }
+}
