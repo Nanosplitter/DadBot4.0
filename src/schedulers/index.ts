b937a49ba8790bd4bbf608ed86fd806b2c99781d
Colin Wilson <github@colinwilson.dev>
2023-03-11 13:03:12 -0500

    fix: :bug: Add apod and firegator to scheduler index

--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -1,7 +1,11 @@
 import { default as remindercheck } from "./remindercheck-scheduler";
 import { default as birthdaycheck } from "./birthdaycheck-scheduler";
+import { default as apod } from "./apod-scheduler";
+import { default as firegator } from "./firegatorThursdays-scheduler";
 
 export default function runSchedulers() {
   remindercheck();
   birthdaycheck();
+  apod();
+  firegator();
 }
