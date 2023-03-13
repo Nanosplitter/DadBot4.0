1c8fc346922da9d2447500f0d916b1d605bdb6df
Colin Wilson <github@colinwilson.dev>
2023-03-13 13:10:26 -0400

    test: :test_tube: Fix bird test to look for URL

--- src/tests/bird.test.ts
+++ src/tests/bird.test.ts
@@ -1,5 +1,6 @@
 import { Bird } from "../commands/bird";
 import type { CommandInteraction } from "discord.js";
+import axios from "axios";
 
 describe("bird", () => {
   it("should return random bird picture", async () => {
@@ -7,8 +8,16 @@ describe("bird", () => {
       reply: jest.fn(),
     };
 
+    axios.get = jest.fn().mockResolvedValue({
+      data: {
+        file: "DummyData.jpg",
+      },
+    });
+
     await Bird.prototype.execute.call({ client: {} }, interaction as unknown as jest.Mocked<CommandInteraction>);
 
     expect(interaction.reply).toHaveBeenCalled();
+
+    expect(axios.get).toHaveBeenCalledWith("http://shibe.online/api/birds");
   });
 });
