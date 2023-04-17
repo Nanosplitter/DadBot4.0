02d56da1fb22a6b4d4cc44523ab34c385235dc5f
Colin Wilson <github@colinwilson.dev>
2023-04-17 10:30:16 -0400

    feat: :sparkles: Add story scheduler

--- src/schedulers/index.ts
+++ src/schedulers/index.ts
@@ -2,10 +2,12 @@ import { default as remindercheck } from "./remindercheck-scheduler";
 import { default as birthdaycheck } from "./birthdaycheck-scheduler";
 import { default as apod } from "./apod-scheduler";
 import { default as firegator } from "./firegatorThursdays-scheduler";
+import { default as pancakechan } from "./pancakechan-scheduler";
 
 export default function runSchedulers() {
   remindercheck();
   birthdaycheck();
   apod();
   firegator();
+  pancakechan();
 }
